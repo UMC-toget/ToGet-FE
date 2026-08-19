399ced000a354cbb81ace86fba73e4406eafabcc
HongYeonLee <llhongyan@naver.com>
2026-08-20 00:53:17 +0900

    fix: 위시 등록/초대장 공유 트래킹 누락 지점 보강
    홈 상품카드 위시 등록(useWishToggle)과 마이페이지/그룹 상세의 초대장 공유
    버튼들에 trackEvent 호출이 빠져 있어 P0-3, P0-8 지표가 집계되지 않던
    문제를 수정한다.

--- src/pages/my/MyGiftListPage.tsx
+++ src/pages/my/MyGiftListPage.tsx
@@ -4,6 +4,7 @@ import FundingListPage from './FundingListPage'
 import MyGiftListCard from './MyGiftListCard'
 import Toast from '../../components/common/Toast'
 import type { MyFunding } from '../../api/users'
+import { trackEvent } from '../../lib/analytics'
 
 const TOAST_DURATION_MS = 2500
 
@@ -17,9 +18,11 @@ export default function MyGiftListPage() {
     try {
       if (navigator.share) {
         await navigator.share({ title: funding.title, url: inviteUrl })
+        trackEvent('invitation_share', { method: 'share', funding_type: 'my' })
         return
       }
       await navigator.clipboard.writeText(inviteUrl)
+      trackEvent('invitation_share', { method: 'copy', funding_type: 'my' })
       setToastMessage('초대장 링크가 복사되었어요')
       setTimeout(() => setToastMessage(null), TOAST_DURATION_MS)
     } catch {
